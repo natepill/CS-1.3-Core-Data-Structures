4c9a94f1042deaa3893797529f6c6dae30e0368a
Nathan Pillai <natepill@gmail.com>
2019-04-03 14:00:55 -0700

    [refactored] inital refactor to encode methodwith some encode test cases passing

--- Lessons/source/bases.py
+++ Lessons/source/bases.py
@@ -1,5 +1,6 @@
 #!python
 import string
+import math
 
 '''
 A base is the max value (non-inclusive) of what the digit can go up to.
@@ -9,6 +10,7 @@ I am going to zip a digits 0-9 and lowercase letters with numbers up to 36 to cr
 nums_and_letters = '{}{}'.format(string.digits, string.ascii_lowercase) # [0-9a-z]
 up_to_36 = (list(range(0, 36))) #list of ints up to 36
 digit_mapper = dict(zip(nums_and_letters, up_to_36)) # maps digit to its decimal value up to 36
+
 '''
 Base 12:
 Example: 36 base12 ===> 42 in base 10
@@ -43,10 +45,12 @@ def decode(digits, base):
     for exponent, digit in enumerate(digits[::-1]):
         if exponent == 0:
             base_10_value += digit_mapper[digit] # On the first index, we are just adding that pure value
+        elif digit.isalpha():
+            base_10_value += (int(base) ** exponent) * int(digit_mapper[digit])
         elif int(digit) == 0: # We want to continue the iteration if 0 is present, because its just a placeholder value
             continue
         else:
-            base_10_value += (base ** exponent) * digit
+            base_10_value += (int(base) ** exponent) * int(digit)
 
     return base_10_value
 
@@ -62,8 +66,9 @@ def decode(digits, base):
     # TODO: Decode digits from any base (2 up to 36)
     # ...
 
-
+# TODO: Go through and replace all "number" variables to their value using dictionary
 def encode(number, base):
+
     """Encode given number in base 10 to digits in given base.
     number: int -- integer representation of number (in base 10)
     base: int -- base to convert to
@@ -72,12 +77,82 @@ def encode(number, base):
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
     # Handle unsigned numbers only for now
     assert number >= 0, 'number is negative: {}'.format(number)
-    # TODO: Encode number in binary (base 2)
-    # ...
-    # TODO: Encode number in hexadecimal (base 16)
-    # ...
-    # TODO: Encode number in any base (2 up to 36)
-    # ...
+
+
+
+    highest_power = math.floor(math.log(number, base))
+    number_string = ''
+    difference = None
+
+
+    for exponent in reversed(range(0, highest_power+1)):
+
+        if number == 0:
+            for num in range(0, len(range(0, exponent))+1):
+                print(num)
+                number_string += '0'
+            break
+
+        elif (base ** exponent) < (number - (base ** exponent)):
+            # print((base ** exponent) < (number - (base ** exponent)))
+            number_string += '0'
+            continue
+
+
+        elif (base ** exponent) == (number - (base ** exponent)): #This means that the current 'index' of the digit representation is = to the base raised to some power
+            divisor = math.floor(number/(base ** exponent)) # How many times does (base ** exponent) go into current value of number?
+            number_string += str(divisor) #The divisor is the digit we're looking for when constructing the returned string
+
+            # Sice base ** exponent is already the current value of number, we can fill the rest of the placeholders with 0's
+            for _ in range(0, len(range(0, exponent))):
+                number_string += '0'
+            break
+
+
+        elif (base ** exponent) > (number - (base ** exponent)):
+
+            divisor = math.floor(number/(base ** exponent)) # How many times does (base ** exponent) go into current value of number?
+            number_string += str(divisor) #The divisor is the digit we're looking for when constructing the returned string
+            number = number - (base ** exponent) # Now that we have the divisor representing in one placeholder value, redo the process with whats left of number
+            print("Number:",number)
+
+
+    return number_string
+
+
+
+
+
+
+    '''Working solution for JUST binary'''
+    # for exponent in reversed(range(0, highest_power+1)):
+    #
+    #     if number == 0:
+    #         for num in range(0, len(range(0, exponent))+1):
+    #             print(num)
+    #             number_string += '0'
+    #         break
+    #     elif (base ** exponent) < (number - (base ** exponent)):
+    #         print((base ** exponent) < (number - (base ** exponent)))
+    #         number_string += '0'
+    #         continue
+    #     elif (base ** exponent) == (number - (base ** exponent)):
+    #         number_string += '1'
+    #         for _ in range(0, len(range(0, exponent))):
+    #             number_string += '0'
+    #         break
+    #
+    #     elif (base ** exponent) > (number - (base ** exponent)):
+    #         print(number)
+    #         number = number - (base ** exponent)
+    #         number_string += '1' # will have to change the added string to accomodate the range of the base values, maybe use ascii mapper
+    #
+    # return number_string
+
+
+
+
+
 
 
 def convert(digits, base1, base2):
@@ -117,5 +192,10 @@ def main():
 
 if __name__ == '__main__':
     # main()
+    # print(decode('10', 2)) # 2
+    # print(decode('9', 10)) # 9
+    # print(decode('10', 10)) # 10
 
-    print(decode('2', 3))
+    # print(decode('a', 16))
+    # print(digit_mapper)
+    print(encode(16, 16))
